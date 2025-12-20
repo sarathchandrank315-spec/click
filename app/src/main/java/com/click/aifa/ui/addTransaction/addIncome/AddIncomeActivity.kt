35b50516c7ffaaa0db342692f24840c4a335ca16
sarathchandrank315-spec <sarathchandrank315@gmail.com>
2025-12-20 16:03:35 +0530

    project creation

--- app/src/main/java/com/click/aifa/ui/addTransaction/addIncome/AddIncomeActivity.kt
+++ app/src/main/java/com/click/aifa/ui/addTransaction/addIncome/AddIncomeActivity.kt
@@ -1,11 +1,15 @@
 package com.click.aifa.ui.addTransaction.addIncome
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import com.click.aifa.data.TransactionEntity
 import com.click.aifa.data.enums.TransactionType
+import com.click.aifa.data.user.UserEntity
+import com.click.aifa.data.user.UserSession
+import com.click.aifa.data.user.UserWithFamily
 import com.click.aifa.databinding.ActivityAddincomeBinding
 import com.click.aifa.viewmodel.IncomeViewModel
 
@@ -23,22 +27,41 @@ class AddIncomeActivity : AppCompatActivity() {
         viewModel = ViewModelProvider(this)[IncomeViewModel::class.java]
         isExpense = intent.getBooleanExtra("IS_EXPENSE", false)
         // Example Selected category:
-        val selectedCategory = "Salary"
+        val selectedCategory = if(isExpense)"income" else "expense"
+        val familyList: MutableList<String> = mutableListOf()
+        UserSession.currentUser?.familyMembers?.map { it.name }?.let {
+            familyList.addAll(it)
+        }
+        UserSession.currentUser?.user?.name?.let {
+            familyList.add(it)
+        }
 
+
+        val adapter = ArrayAdapter(
+            this,
+            android.R.layout.simple_dropdown_item_1line,
+            familyList
+        )
+
+        binding.editEarner.setAdapter(adapter)
+        binding.editEarner.keyListener = null      // disable typing
+        binding.editEarner.setOnClickListener {
+            binding.editEarner.showDropDown()       // force show dropdown
+        }
         binding.btnAddIncome.setOnClickListener {
 
             val title = binding.editIncomeTitle.text.toString()
             val amount = binding.editIncome.text.toString().toDoubleOrNull() ?: 0.0
+            val payeePayer = binding.editEarner.text.toString()
             val currentDate = System.currentTimeMillis()
-
             val income = TransactionEntity(
-                user = "sarath",
+                user = UserSession.currentUser?.user.toString(),
                 title = title,
                 amount = amount,
                 category = selectedCategory,
                 date = currentDate,
                 type = if (isExpense) TransactionType.EXPENSE else TransactionType.INCOME,
-                payeePayer = "Sarath"
+                payeePayer = payeePayer
             )
 
             viewModel.insertIncome(income)
